986f5a04ec9d27d9ca4f98113550e92f9d63985f
Jenni <winegaj86@gmail.com>
2025-05-14 17:31:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 // Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, world! My server is ACTUALLY running');
+  res.send('Hello, world! Your server is running');
 });
 
 // Start the server
